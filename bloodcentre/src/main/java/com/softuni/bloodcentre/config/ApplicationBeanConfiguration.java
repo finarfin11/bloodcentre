8e0f9b4efaa92aa4fed084bf73502dd99a9379ab
finarfin11 <la.finarfin@gmail.com>
2019-12-13 16:38:44 +0200

    html improvements, added log filter

--- bloodcentre/src/main/java/com/softuni/bloodcentre/config/ApplicationBeanConfiguration.java
+++ bloodcentre/src/main/java/com/softuni/bloodcentre/config/ApplicationBeanConfiguration.java
@@ -5,6 +5,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 public class ApplicationBeanConfiguration {
@@ -19,5 +20,15 @@ public class ApplicationBeanConfiguration {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(false);
+        return loggingFilter;
     }
 }
